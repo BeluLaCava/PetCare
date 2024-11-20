519d27d39c6e500f1779fbe850c6f7d0ba7466b0
mardav82 <mardav82@hotmail.com.ar>
2024-11-20 19:20:52 -0300

    Archivo Veterinario

---
+++ PetCare/BLL/VeterinarioBusiness.cs
@@ -0,0 +1,139 @@
+﻿using DAL;
+using Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Transactions;
+
+namespace BLL
+{
+    public class VeterinarioBusiness
+    {
+        private VeterinarioData veterinarioDAL = new VeterinarioData();
+
+        public void Guardar(Veterinario veterinario)
+        {
+            try
+            {
+                using (TransactionScope trx = new TransactionScope())
+
+                {
+
+                    if (string.IsNullOrEmpty(veterinario.Nombre))
+                        throw new Exception("El nombre no puede estar vacío.");
+                    if (string.IsNullOrEmpty(veterinario.Especialidad))
+                        throw new Exception("La especialidad no puede estar vacía.");
+                    if (string.IsNullOrEmpty(veterinario.HorarioTrabajo))
+                        throw new Exception("El horario de trabajo no puede estar vacío.");
+
+                    veterinarioDAL.GuardarVeterinario(veterinario);
+                    trx.Complete();
+                }
+            }
+
+            catch (Exception ex)
+            {
+
+                throw;
+            }
+        }
+
+        public void GuardarVeterinarios(List<Veterinario> veterinario)
+        {
+            try
+            {
+                using (TransactionScope trx = new TransactionScope())
+                {
+
+
+                    foreach (Veterinario veterinarios in veterinario)
+                    {
+                        Guardar(veterinarios);
+                    }
+                    trx.Complete();
+                }
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
+        }
+
+     
+
+        public List<Veterinario> ObtenerVeterinario()
+        {
+            try
+            {
+                return VeterinarioData.ObtenerVeterinario();
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
+        }
+        public void ModificarVeterinario(int id, string nombre, string especialidad, string horariodetrabajo)
+        {
+            try
+            {
+                using (TransactionScope trx = new TransactionScope())
+                {
+                    Veterinario veterinario = veterinarioDAL.GetById(id);
+                    if (veterinario == null)
+                    {
+                        throw new Exception("El veterinario no existe");
+                    }
+                    if (string.IsNullOrEmpty(veterinario.Nombre))
+                    {
+                        throw new Exception("El nombre no puede estar vacío.");
+                    }
+                    if (string.IsNullOrEmpty(veterinario.Especialidad))
+                    {
+                        throw new Exception("La especialidad no puede estar vacía.");
+                    }
+                    if (string.IsNullOrEmpty(veterinario.HorarioTrabajo))
+                    {
+                        throw new Exception("El horario de trabajo no puede estar vacío.");
+                    }
+                    veterinario.Nombre = nombre;
+                    veterinario.Especialidad = especialidad;
+                    veterinario.HorarioTrabajo = horariodetrabajo;
+
+                    trx.Complete();
+                }
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
+        }
+
+        public void EliminarVeterinario(int id)
+        {
+            try
+            {
+                using (TransactionScope trx = new TransactionScope())
+                {
+                    Veterinario veterinario = veterinarioDAL.GetById(id);
+                    if (veterinario == null)
+                    {
+                        throw new Exception("El Veterinario no existe");
+                    }
+                    veterinarioDAL.EliminarVeterinario(id);
+                    trx.Complete();
+                }
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
+        }
+
+    }
+}
--- PetCare/Entity/Veterinario.cs
+++ PetCare/Entity/Veterinario.cs
@@ -8,6 +8,7 @@ namespace Entity
 {
     public class Veterinario
     {
+
         public int ID { get; set; }
         public string Nombre { get; set; }
         public string Especialidad { get; set; }
