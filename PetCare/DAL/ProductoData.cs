0cbf2d2657297efce15e006f41688dca60c3e7b9
EnzoMansilla <mansillae217@gmail.com>
2024-11-21 03:29:47 -0300

    Funcion de Facturas y Productos

---
+++ PetCare/DAL/ProductoData.cs
@@ -0,0 +1,123 @@
+﻿using Entity;
+using Mapper;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public  class ProductoData
+    {
+        public void GuardarProducto(Producto prod)
+        {
+            SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["PetCareDB"].ConnectionString);
+            try
+            {
+
+                using (conn)
+                {
+                    conn.Open();
+                    SqlCommand cmd = new SqlCommand(@"INSERT INTO Productos (nombre, descripcion, precio, stock) 
+                                                    VALUES (@nombre, @descripcion, @precio, @stock)", conn);
+                    using (cmd)
+                    {
+                        cmd.Parameters.AddWithValue("@nombre", prod.Nombre);
+                        cmd.Parameters.AddWithValue("@descipcion", prod.Descripcion);
+                        cmd.Parameters.AddWithValue("@precio", prod.Precio);
+                        cmd.Parameters.AddWithValue("@stock", prod.Stock);                        
+                        cmd.ExecuteNonQuery();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
+
+        }
+        public List<Producto> ObtenerProducto()
+        {
+            List<Producto> listaProducto = new List<Producto>();
+            SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["PetCareDB"].ConnectionString);
+            try
+            {
+                using (conn)
+                {
+                    conn.Open();                    
+                    using (SqlCommand cmd = new SqlCommand("SELECT nombre, descripcion, precio, stock, ID FROM Productos", conn))
+                    {
+                        
+                        using (SqlDataReader reader = cmd.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                Producto producto = ProductoMapper.Map(reader);
+                                listaProducto.Add(producto);
+                            }
+
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+            return listaProducto;
+        }
+
+        public void ModificarProducto(Producto producto)
+        {
+            SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["PetCareDB"].ConnectionString);
+            try
+            {
+                using (conn)
+                {
+                    conn.Open();
+                    SqlCommand cmd = new SqlCommand("UPDATE Productos SET nombre = @nombre, descripcion = @descripcion, precio = @precio, stock = @stock where ID = @idProducto", conn);
+                    using (cmd)
+                    {
+                        cmd.Parameters.AddWithValue("@nombre", producto.Nombre);
+                        cmd.Parameters.AddWithValue("@descripcion", producto.Descripcion);
+                        cmd.Parameters.AddWithValue("@precio", producto.Precio);
+                        cmd.Parameters.AddWithValue("@stock", producto.Stock);
+                        cmd.Parameters.AddWithValue("@idProducto", producto.ID);
+                        cmd.ExecuteNonQuery();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
+        }
+        public void EliminarProducto(int idProducto)
+        {
+            SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["PetCareDB"].ConnectionString);
+            try
+            {
+                using (conn)
+                {
+                    conn.Open();
+                    SqlCommand cmd = new SqlCommand("delete from Productos where ID = @idProducto", conn);
+                    using (cmd)
+                    {
+                        cmd.Parameters.AddWithValue("@idProducto", idProducto);
+                        cmd.ExecuteNonQuery();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
+        }
+    }
+}
--- PetCare/UI/FrmFactura.cs
+++ PetCare/UI/FrmFactura.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using BLL;
+using Entity;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,10 +14,47 @@ namespace UI
 {
     public partial class FrmFactura : Form
     {
+
+        ClienteBusiness clienteBusiness = new ClienteBusiness();
+        FacturaBusiness facturaBusiness = new FacturaBusiness();
+        ProductoBusiness productoBusiness = new ProductoBusiness();
         public FrmFactura()
         {
             InitializeComponent();
+            actualizarCampos();
         }
+
+        private void actualizarCampos()
+        {
+            actualizarCombo();
+            updateDataGridView();
+        }
+        private void actualizarCombo()
+        {
+            cmbCliente.DataSource = clienteBusiness.ObtenerCliente();
+            cmbCliente.ValueMember = "ID";
+            cmbCliente.DisplayMember = "Nombre";
+
+            cmbProducto.DataSource = productoBusiness.ObtenerProductos();
+            cmbProducto.ValueMember = "ID";
+            cmbProducto.DisplayMember = "Nombre";
+
+            cmbModClie.DataSource = clienteBusiness.ObtenerCliente();
+            cmbModClie.ValueMember = "ID";
+            cmbModClie.DisplayMember = "Nombre";
+
+            cmbModProd.DataSource = productoBusiness.ObtenerProductos();
+            cmbModProd.ValueMember = "ID";
+            cmbModProd.DisplayMember = "Nombre";
+
+        }
+
+        private void updateDataGridView()
+        {
+            dataGridView1.DataSource = null;
+            dataGridView1.DataSource = facturaBusiness.obtenerFacturas();
+        }
+
 
         private void FrmFactura_Load(object sender, EventArgs e)
         {
@@ -29,6 +68,70 @@ namespace UI
 
         private void label5_Click(object sender, EventArgs e)
         {
+
+        }
+
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Factura fac = new Factura();
+                fac.Cantidad = Convert.ToInt32(txtCantidad.Text);
+                fac.Producto = new Producto();
+                fac.Producto.ID = Convert.ToInt32(cmbProducto.SelectedValue);
+                fac.Cliente = new Cliente();
+                fac.Cliente.ID = Convert.ToInt32(cmbCliente.SelectedValue);
+                fac.Fecha = DateTime.Parse(dateFechaValidez.Text);
+                facturaBusiness.agregarFactura(fac);
+                updateDataGridView();
+                MessageBox.Show("Factura agregada");
+            }
+            catch (Exception ex) { MessageBox.Show(ex.Message); }
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnModificar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Factura fac = new Factura();
+                fac.ID = Convert.ToInt32(txtIdMod.Text);
+                fac.Producto = new Producto();
+                fac.Producto.ID = Convert.ToInt32(cmbModProd.SelectedValue);
+                fac.Cliente = new Cliente();
+                fac.Cliente.ID = Convert.ToInt32(cmbModClie.SelectedValue);
+                fac.Cantidad = Convert.ToInt32(txtModCant.Text);
+                facturaBusiness.modificarFactura(fac);
+                updateDataGridView();
+                MessageBox.Show("Factura modificada");
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            
+
+        }
+
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int id = Convert.ToInt32(txtIdEliminar.Text);
+                facturaBusiness.eliminarFactura(id);
+                updateDataGridView();
+                MessageBox.Show("Factura Eliminada");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
 
         }
     }
